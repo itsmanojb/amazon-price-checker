0365860368e641896d3e2979d135df8a4dec451f
Manoj Barman <the_manoj@live.com>
2019-12-17 23:23:15 +0530

    Parser file added

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Amazon Price Checker
 A Node.js application to track Amazon's prices of a given product.
+
+### How to check
+
+Run the follwoing command:
+<pre>
+$ node parser.js [amazon_product_id] [price]
+</pre>
+
+### Example
+- Visit amazon.in and grab the product id.
+
+- Generally amazon products have url like, _www.amazon .in/dp/[productId]/random_tracking_staffs_ or _www.amazon .in/[product_name]/dp/[productId]/random_tracking_staffs_
+- Run following command:
+  
+
+<pre>
+
+
+$ node parser.js B07JGXM9WN 40000
+
+Apple iPhone XR (64GB) - White is still expensive. Current Price ₹ 49,900.00
+
+</pre>
---
+++ parser.js
@@ -0,0 +1,49 @@
+const nightmare = require('nightmare')()
+
+const args = process.argv.slice(2)
+const productId = args[0]
+const minPrice = args[1]
+
+const rod = (function rod() {
+  const chars = "|/-\\";
+  let i = 0;
+  return function () {
+    i = (i + 1) % 4;
+    process.stdout.write(`Checking ${chars[i]}\r`);
+  }
+})()
+
+const loading = setInterval(rod, 100)
+checkPrice()
+
+async function checkPrice() {
+  try {
+    const url = `https://www.amazon.in/dp/${productId}`;
+    const data = await nightmare.goto(url)
+      .wait("#priceblock_ourprice")
+      .evaluate(() => {
+        const parsedArr = [];
+        parsedArr.push(document.getElementById("productTitle").innerText)
+        const priceText = document.getElementById("priceblock_ourprice").innerText;
+        if (!priceText.includes('-')) {
+          parsedArr.push(priceText)
+        }
+        return parsedArr;
+      })
+      .end()
+    if (data.length == 1) {
+      console.log(`\n\nFailed to determine actual price of ${data[0]}.\n`)
+    } else {
+      const priceNumber = parseFloat(data[1].replace(/[₹, ]/g, ''))
+      if (priceNumber < minPrice) {
+        console.log(`\n\nThe price of ${data[0]} has dropped. Current Price ${data[1]}\n`)
+      } else {
+        console.log(`\n\n${data[0]} is still expensive. Current Price ${data[1]}\n`)
+      }
+    }
+    clearInterval(loading)
+  } catch (e) {
+    console.log('Amazon Price Checker Error', e.message)
+    throw e
+  }
+}
